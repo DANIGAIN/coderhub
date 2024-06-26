2a776c131a44242ca1a8afae8e1ab53ee5579f69
DANIGAIN <danigain1234@gmail.com>
2024-06-26 20:55:34 +0600

    add my proposal

--- src/app/(main)/service/[id]/page.js
+++ src/app/(main)/service/[id]/page.js
@@ -20,41 +20,24 @@ function servicePage(props) {
   return (
     <div className='bg-slate-900 '>
       <Section1 service={service} />
-      <div className="mt-20 max-w-4xl ">
-        <ul className="flex border-b">
-          <li className="text-gray-800 font-semibold text-sm bg-gray-100 py-3 px-8 border-b-2 border-gray-800 cursor-pointer transition-all">
-            Description
-          </li>
-          <li className="text-gray-500 font-semibold text-sm hover:bg-gray-100 py-3 px-8 cursor-pointer transition-all">
-            Reviews
-          </li>
-        </ul>
-        <div className="mt-8">
-          <h3 className="text-xl font-bold text-gray-800">Product Description</h3>
-          <p className="text-sm text-gray-500 mt-4">
-            Elevate your casual style with our premium men's t-shirt. Crafted for
-            comfort and designed with a modern fit, this versatile shirt is an
-            essential addition to your wardrobe. The soft and breathable fabric
-            ensures all-day comfort, making it perfect for everyday wear. Its classic
-            crew neck and short sleeves offer a timeless look.
-          </p>
+      <div className='mx-5 sm:mx-10 lg:mx-70 md:50'>
+        <div className="mt-20 max-w-4xl ">
+          <ul className="flex border-b">
+            <li className="text-gray-800 font-semibold text-sm bg-gray-100 py-3 px-8 border-b-2 border-gray-800 cursor-pointer transition-all">
+              Description
+            </li>
+          </ul>
+          <div className="mt-8">
+            <h3 className="text-xl font-bold text-gray-800">Service Description</h3>
+            <p className="text-sm text-gray-500 mt-4">
+              {service?.category?.description}
+            </p>
+          </div>
+
         </div>
-        <ul className="space-y-3 list-disc mt-6 pl-4 text-sm text-gray-500">
-          <li>A gray t-shirt is a wardrobe essential because it is so versatile.</li>
-          <li>
-            Available in a wide range of sizes, from extra small to extra large, and
-            even in tall and petite sizes.
-          </li>
-          <li>
-            This is easy to care for. They can usually be machine-washed and dried on
-            low heat.
-          </li>
-          <li>
-            You can add your own designs, paintings, or embroidery to make it your
-            own.
-          </li>
-        </ul>
+
       </div>
+
 
       <Review />
     </div>
--- src/app/(main)/service/my-proposal/page.js
+++ src/app/(main)/service/my-proposal/page.js
@@ -2,6 +2,88 @@ import React from 'react'
 
 export default function MyProposal() {
   return (
-    <div>MyProposal</div>
+    <div className=" bg-slate-900  p-8 rounded-md w-full">
+      <div className=" flex items-center justify-between pb-6">
+        <div >
+          <h2 className="text-gray-600 font-semibold">List of proposals </h2>
+          <span className="text-xs">Only for your</span>
+        </div>
+
+      </div>
+      <div>
+        <div className="-mx-4 sm:-mx-8 px-4 sm:px-8 py-4 overflow-x-auto">
+          <div className="inline-block min-w-full shadow rounded-lg overflow-hidden">
+            <table className="min-w-full leading-normal">
+              <thead>
+                <tr>
+                  <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                    SR
+                  </th>
+                  <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                    Type
+                  </th>
+                  <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                    Days
+                  </th>
+                  <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                    Title
+                  </th>
+                  <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                    Description
+                  </th>
+                  <th className="px-5 py-3 border-b-2 border-gray-200 bg-gray-100 text-left text-xs font-semibold text-gray-600 uppercase tracking-wider">
+                    Status
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td className="px-5 py-5 border-b border-gray-200 text-sm bg-slate-600">
+                    <div className="flex items-center">
+                      <div className="flex-shrink-0 w-10 h-10">
+                        <img
+                          className="w-full h-full rounded-full"
+                          src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2.2&w=160&h=160&q=80"
+                          alt=""
+                        />
+                      </div>
+                      <div className="ml-3">
+                        <p className="text-gray-900 whitespace-no-wrap">
+                          Vera Carpenter
+                        </p>
+                      </div>
+                    </div>
+                  </td>
+                  <td className="px-5 py-5 border-b border-gray-200 bg-slate-600  text-sm">
+                    <p className="text-gray-900 whitespace-no-wrap">Admin</p>
+                  </td>
+                  <td className="px-5 py-5 border-b border-gray-200 bg-slate-600 text-sm">
+                    <p className="text-gray-900 whitespace-no-wrap">Jan 21, 2020</p>
+                  </td>
+                  <td className="px-5 py-5 border-b border-gray-200 bg-slate-600 text-sm">
+                    <p className="text-gray-900 whitespace-no-wrap">43</p>
+                  </td>
+                  <td className="px-5 py-5 border-b border-gray-200 bg-slate-600 text-sm">
+                    <p className="text-gray-900 whitespace-no-wrap">43</p>
+                  </td>
+                  <td className="px-5 py-5 border-b border-gray-200 bg-slate-600 text-sm">
+                    <span className="relative inline-block px-3 py-1 font-semibold text-green-900 leading-tight">
+                      <span
+                        aria-hidden=""
+                        className="absolute inset-0 bg-green-200 opacity-50 rounded-full"
+                      />
+                      <span className="relative">Activo</span>
+                    </span>
+                  </td>
+                </tr>
+            
+              </tbody>
+            </table>
+        
+          </div>
+        </div>
+      </div>
+    </div>
+
   )
 }
