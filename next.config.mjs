217d379285f0fd76b1b4ecb8ec26df04dafcc798
DANIGAIN <danigain1234@gmail.com>
2024-08-09 17:03:22 +0600

    something changes

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,16 @@ const nextConfig = {
     images: {
         domains: ['lh3.googleusercontent.com','avatars.githubusercontent.com' ,'i.imgur.com','www.google.com','www.gadstyle.com'], 
       },
-
+    env: {
+        MONGO_URI: process.env.MONGO_URI,
+        GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+        GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+        GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
+        GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
+        SECRET: process.env.SECRET,
+        GMAIL: process.env.GMAIL,
+        APP_PASSWORD: process.env.APP_PASSWORD,
+      },
 };
 
 export default nextConfig;
