0a3731ee71433fe326830ade5e619030b8e42b16
DANIGAIN <danigain1234@gmail.com>
2024-08-11 07:32:39 +0600

    some one chnahes

--- src/middleware.js
+++ src/middleware.js
@@ -18,17 +18,20 @@ export async function middleware(req) {
     ) {
         return NextResponse.redirect(new URL('/home', req.url))
     }
+    if(!token){
+        return NextResponse.rewrite(new URL('/not-found', req.url))
+    }
     // permission ---------->>        
     if (pathname.startsWith('/agency/dashboard')) {
-        // const url = new URL('/api/mapings',req.url);
-        // url.searchParams.set('role', token.role);
-        // url.searchParams.set('pathname', pathname);
-        // const fullUrl = url.href;
-        // const response = await  fetch(fullUrl)
-        // const res =  await response.json()
-        // if(!res.data){
-        //     return NextResponse.rewrite(new URL('/not-found', req.url))
-        // }        
+        const url = new URL('/api/mapings',req.url);
+        url.searchParams.set('role', token.role);
+        url.searchParams.set('pathname', pathname);
+        const fullUrl = url.href;
+        const response = await  fetch(fullUrl)
+        const res =  await response.json()
+        if(!res.data){
+            return NextResponse.rewrite(new URL('/not-found', req.url))
+        }              
         return NextResponse.next()
     }
 
